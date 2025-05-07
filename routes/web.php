6b9668232699b1750b62f754e6d31b03597f227f
matin4kateb <matin.kateb.mk@gmail.com>
2025-05-07 09:13:03 +0330

    Added 'auth' middleware for some routes.

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,26 @@ Route::get('contact', [GeneralController::class, 'contact'])->name('contact');
 Route::get('about', [GeneralController::class, 'about'])->name('about');
 Route::get('faq', [GeneralController::class, 'faq'])->name('faq');
 
-Route::get('product/add', [ProductController::class, 'add'])->name('product.add');
-// Route::get('product/{product}/delete', [ProductController::class, 'delete'])->name('product.delete');
-Route::get('product/{product}/update', [ProductController::class, 'update'])->name('product.update');
-Route::get('product/view', [ProductController::class, 'viewProds'])->name('product.view');
 
-Route::put('product/{product}/update', [ProductController::class, 'updateAction']);
-Route::delete('product/{product}/delete', [ProductController::class, 'deleteAction'])->name('product.delete');
-Route::post('product/add', [ProductController::class, 'addAction'])->name('product.add.action');
 
 Route::get('login', [LoginController::class, 'show'] )->name('login');
 Route::post('login', [LoginController::class, 'login'] );
 
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('register', [RegisterController::class, 'show'] )->name('register');
 Route::post('register', [RegisterController::class, 'store'] );
 
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');  // Logout
+
+Route::middleware('auth')->group(function() {
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');  // Logout
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('product/add', [ProductController::class, 'add'])->name('product.add');
+    // Route::get('product/{product}/delete', [ProductController::class, 'delete'])->name('product.delete');
+    Route::get('product/{product}/update', [ProductController::class, 'update'])->name('product.update');
+    Route::get('product/view', [ProductController::class, 'viewProds'])->name('product.view');
+
+    Route::put('product/{product}/update', [ProductController::class, 'updateAction']);
+    Route::delete('product/{product}/delete', [ProductController::class, 'deleteAction'])->name('product.delete');
+    Route::post('product/add', [ProductController::class, 'addAction'])->name('product.add.action');
+});
